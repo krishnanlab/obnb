785bcad313b4f21d483bae74afbee0054bbc69b2
Remy <liurenmi@msu.edu>
2019-03-05 14:33:53 -0500

    Change method name, remove ID2idx and IDary2idxary, replace by __getitem__

--- src/NLEval/unit_tests/test_util/IDmap_test.py
+++ src/NLEval/unit_tests/test_util/IDmap_test.py
@@ -4,41 +4,41 @@ from IDmap import IDmap
 class TestIDmap(unittest.TestCase):
 	def setUp(self):
 		self.IDmap = IDmap()
-		self.IDmap.newID('a')
+		self.IDmap.addID('a')
 
 	def tearDown(self):
 		self.IDmap = None
 
 	def test_size(self):
 		self.assertEqual(self.IDmap.size, 1)
-		self.IDmap.newID('b')
+		self.IDmap.addID('b')
 		self.assertEqual(self.IDmap.size, 2)
 
 	def test_data(self):
 		self.assertEqual(self.IDmap.data, {'a':0})
-		self.IDmap.newID('b')
+		self.IDmap.addID('b')
 		self.assertEqual(self.IDmap.data, {'a':0,'b':1})
 
 	def test_lst(self):
 		self.assertEqual(self.IDmap.lst, ['a'])
-		self.IDmap.newID('b')
+		self.IDmap.addID('b')
 		self.assertEqual(self.IDmap.lst, ['a','b'])
 
 	def test_newID(self):
-		self.assertRaises(AssertionError, self.IDmap.newID, 'a')
+		self.assertRaises(AssertionError, self.IDmap.addID, 'a')
 
-	def test_ID2idx(self):
-		self.assertEqual(self.IDmap.ID2idx('a'), 0)
+	def test_getitem(self):
+		self.assertEqual(self.IDmap['a'], 0)
 
 	def test_idx2ID(self):
 		self.assertEqual(self.IDmap.idx2ID(0), 'a')
 
 	def test_IDary2idxary(self):
-		self.IDmap.newID('b')
-		self.assertEqual(self.IDmap.IDary2idxary(['b','a'])[0], 1)
-		self.assertEqual(self.IDmap.IDary2idxary(['b','a'])[1], 0)
-		self.assertEqual(self.IDmap.IDary2idxary(['a','b'])[0], 0)
-		self.assertEqual(self.IDmap.IDary2idxary(['a','b'])[1], 1)
+		self.IDmap.addID('b')
+		self.assertEqual(self.IDmap[['b','a']][0], 1)
+		self.assertEqual(self.IDmap[['b','a']][1], 0)
+		self.assertEqual(self.IDmap[np.array(['a','b'])][0], 0)
+		self.assertEqual(self.IDmap[np.array(['a','b'])][1], 1)
 
 
 if __name__ == '__main__':
