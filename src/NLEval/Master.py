70e5695e3e8e303d93125f01e55bc395cd0fd0ef
Remy <liurenmi@msu.edu>
2019-07-23 10:44:02 -0400

    update

---
+++ src/NLEval/Master.py
@@ -0,0 +1,49 @@
+
+
+class Master:
+	def __init__( self ):
+		pass
+
+	def __repr__( self ):
+		pass
+
+	def __str__( self ):
+		pass
+
+	def get_all_graphs( self ):
+		pass
+
+	def graph_stats_table( self ):
+		pass
+
+	def get_label( self, drop_thresh=None, spec_thresh=None ):
+		'''
+		Import labels and filter on the fly
+		'''
+		pass
+
+	def get_node_property( self ):
+		pass
+
+	def get_com_id_set( self ):
+		pass
+
+	def eval_worker( self ):
+		pass
+
+	def get_scores( self ):
+		pass
+
+	def get_mdl_info( self ):
+		pass
+
+	def score_cut_off( self, cut_off_dict ):
+		pass
+
+	def score_summary( self ):
+		pass
+
+	def score_details( self ):
+		pass
+
+
---
+++ src/NLEval/wrapper/test_ParDat.py
@@ -0,0 +1,13 @@
+from NetGSEA.wrapper import ParWrap
+import time
+
+lst = list(range(20))
+@ParWrap.ParDat(lst)
+def test(a,b,c):
+	time.sleep(2)
+	#print("a=%s"%repr(a))
+	return a
+
+for i in test(b=3,c=5):
+	print(i)
+
