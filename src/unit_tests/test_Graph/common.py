957269de9811951b9e4e48149f7d6ee33ac85526
Renming Liu <renmingliu@host-178660.dhcp.egr.msu.edu>
2019-07-23 12:44:34 -0400

    Find absolute path to source folder

--- src/unit_tests/test_Graph/common.py
+++ src/unit_tests/test_Graph/common.py
@@ -1,5 +1,10 @@
+import os
 from sys import path
-path.append('../../')
+pwd = os.path.dirname(os.path.abspath(__file__))
+lst = pwd.split('/')
+idx = lst.index('src') + 1
+src_pth = '/'.join(lst[:idx])
+path.append(src_pth)
 from NLEval.util import IDmap
 import unittest
 import numpy as np
