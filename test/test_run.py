e7a266b5fb623fb4995f072076aa2c462e43b357
Renming Liu <liurenmi@msu.edu>
2019-08-04 15:35:50 -0400

    Test run using L2-LosgisticRegression for KEGGBP on STRING-EXP using study-bias holdout

---
+++ test/test_run.py
@@ -0,0 +1,28 @@
+from sys import path
+path.append('../src/')
+from NLEval import graph, valsplit, label, model
+from sklearn.metrics import roc_auc_score as auroc
+import numpy as np
+
+g = graph.DenseGraph.DenseGraph.from_edglst(\
+	'../data/networks/STRING-EXP.edg', weighted=True, directed=False)
+lsc = label.LabelsetCollection.SplitLSC.from_gmt('../data/labels/KEGGBP.gmt')
+lsc.load_entity_properties('../data/properties/pubcnt.txt', \
+		'Pubmed Count', 0, int)
+lsc.valsplit = valsplit.Holdout.BinHold(3, shuffle=True)
+lsc.train_test_setup(g, prop_name='Pubmed Count', min_pos=10)
+
+mdl = model.SupervisedLearning.LogReg(g, penalty='l2', solver='lbfgs')
+
+for labelID in lsc.labelIDlst:
+	print('%.4f\t%s'%(auroc(*(mdl.test(lsc.splitLabelset(labelID)))), labelID))
+
+"""
+#for printing average properties in training/testing sets
+for labelID in lsc.labelIDlst:
+	tr, _, ts, _ = next(lsc.splitLabelset(labelID))
+	avg_tr_prop = np.array([lsc.entity.getProp(i, 'Pubmed Count') for i in tr]).mean()
+	avg_ts_prop = np.array([lsc.entity.getProp(i, 'Pubmed Count') for i in ts]).mean()
+	print('Avg train prop: %.6f\t Avg test prop: %.6f'%\
+		(avg_tr_prop, avg_ts_prop))
+"""
