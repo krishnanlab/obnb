4134f00182b693ccc768e2d2e0e96ca7825b4eb4
Renming Liu <liurenmi@msu.edu>
2019-08-29 20:17:35 -0400

    Fix path issue

--- src/test_runs/test_SL.py
+++ src/test_runs/test_SL.py
@@ -1,13 +1,15 @@
 from sys import path
-path.append('../src/')
+path.append('../')
 from NLEval import graph, valsplit, label, model
 from sklearn.metrics import roc_auc_score as auroc
 import numpy as np
 
-g = graph.DenseGraph.DenseGraph.from_edglst(\
-	'../data/networks/STRING-EXP.edg', weighted=True, directed=False)
-lsc = label.LabelsetCollection.SplitLSC.from_gmt('../data/labels/KEGGBP.gmt')
-lsc.load_entity_properties('../data/properties/pubcnt.txt', \
+data_path = '../../data/'
+
+g = graph.DenseGraph.DenseGraph.from_edglst(data_path \
+	+ 'networks/STRING-EXP.edg', weighted=True, directed=False)
+lsc = label.LabelsetCollection.SplitLSC.from_gmt(data_path + 'labels/KEGGBP.gmt')
+lsc.load_entity_properties(data_path + '/properties/pubcnt.txt', \
 		'Pubmed Count', 0, int)
 lsc.valsplit = valsplit.Holdout.BinHold(3, shuffle=True)
 lsc.train_test_setup(g, prop_name='Pubmed Count', min_pos=10)
