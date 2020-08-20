cb1e97b4e0d494a8883db4404b4e0136d55e7851
Renming Liu <liurenmi@msu.edu>
2020-08-20 09:46:04 -0400

    Fix input when creating valsplit objects

--- src/NLEval/valsplit/Interface.py
+++ src/NLEval/valsplit/Interface.py
@@ -13,19 +13,19 @@ class sklInterface(BaseInterface):
 class sklSKF(sklInterface):
 	"""Dedicated interface for Stratified K-Fold in SKLearn"""
 	def __init__(self, skl_kws={}, shuffle=False):
-		super(sklSKF, self).__init__(ms.StratifiedKFold)
+		super(sklSKF, self).__init__(ms.StratifiedKFold, skl_kws, shuffle)
 
 class sklSSS(sklInterface):
 	"""Dedicated interface for Stratified Shuffle Split in SKLearn"""
 	def __init__(self, skl_kws={}, shuffle=False):
-		super(sklSSS, self).__init__(ms.StratifiedShuffleSplit)
+		super(sklSSS, self).__init__(ms.StratifiedShuffleSplit, skl_kws, shuffle)
 
 class sklLOO(sklInterface):
 	"""Dedicated interface for Leave One Out in SKLearn"""
 	def __init__(self, skl_kws={}, shuffle=False):
-		super(sklLOO, self).__init__(ms.LeavePOut)
+		super(sklLOO, self).__init__(ms.LeavePOut, skl_kws, shuffle)
 
 class sklLPO(sklInterface):
 	"""Dedicated interface for Leave P Out in SKLearn"""
 	def __init__(self, skl_kws={}, shuffle=False):
-		super(sklLPO, self).__init__(ms.LeaveOneOu)
+		super(sklLPO, self).__init__(ms.LeaveOneOu, skl_kws, shuffle)
