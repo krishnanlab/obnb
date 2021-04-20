1e47bba4e942894b915e6017f41e9eeeaca1a4b5
Remy <liurenmi@msu.edu>
2021-04-20 06:48:09 -0400

    Fixed prediction bug in training adaboost; retrain model in each iteration using sample weights

--- src/NLEval/model/SupervisedLearning.py
+++ src/NLEval/model/SupervisedLearning.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
 from sklearn.svm import LinearSVC
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 from NLEval.model.BaseModel import BaseModel
 
 __all__ = ['SLBase', 'LogReg', 'SVM', 'RF']
@@ -35,7 +36,7 @@ class CombSLBase(BaseModel):
 	def train(self, ID_ary, y):
 		for i, mat in enumerate(self.G.mat_list):
 			x = mat[self.G.IDmap[ID_ary]]
-			self.mdl_list[i].fit(x,y)
+			self.mdl_list[i].fit(x, y)
 		self.fit_master_mdl(ID_ary, y)
 
 class CombLogRegCVBagging(CombSLBase):
@@ -65,13 +66,13 @@ class CombLogRegCVAdaBoost(CombSLBase):
 		selected_ind = np.zeros(n_mdl, dtype=bool)  # inidvator for selected model
 		w = np.ones(len(ID_ary)) / len(ID_ary)  # data point weights
 		coef = np.zeros(n_mdl)  # model boosting coefficients
-		y_pred_mat = np.zeros((len(ID_ary), n_mdl))  # predictions from all models
+		y_pred_mat = np.zeros((len(ID_ary), n_mdl), dtype=bool)  # predictions from all models
 
 		# generate predictions from individual models
 		idx_ary = self.G.IDmap[ID_ary]
 		for i, mdl in enumerate(self.mdl_list):
-			            x = self.G.mat_list[i][idx_ary]
-			            y_pred_mat[:,i] = mdl.predict(x)
+			x = self.G.mat_list[i][idx_ary]
+			y_pred_mat[:,i] = mdl.predict(x)
 
 		# determine boosting coefficients
 		for i in range(n_mdl):
@@ -81,19 +82,29 @@ class CombLogRegCVAdaBoost(CombSLBase):
 			for j in range(n_mdl):
 				if selected_ind[j]:
 					continue
-				err = w[y_pred_mat[:,i] != y].sum()
+
+				# retrain model using sample weights
+				mdl = self.mdl_list[j]
+				x = self.G.mat_list[j][idx_ary]
+				mdl.fit(x, y, sample_weight=w)
+				y_pred_mat[:,j] = mdl.predict(x)
+
+				err = w[y_pred_mat[:,j] != y].sum()
 				if err < opt_err:
 					opt_err = err
 					opt_idx = j
 
 			a = 0.5 * np.log((1 - opt_err) / opt_err)  # model coefficient
-			if a < 0: print(f"Warning: encountered worse than random prediction, a = {a}, set to 0")
+			if a < 0: 
+				print(f"Warning: encountered worse than random prediction, a = {a}, set to 0")
+				a = 0
 			y_pred_opt = y_pred_mat[:, opt_idx] == 1  # predictions of optimal model
 			w[y_pred_opt == y] *= np.exp(-a)  # down weight correct predictions
 			w[y_pred_opt != y] *= np.exp(a)  # up weight incorrect predictions
 			w /= w.sum()  # normalize data point weights
 			selected_ind[opt_idx] = True  # remove selected model from candidates
-			#coef[opt_idx] = max(a, 0)
+			coef[opt_idx] = a
+			#print(f"Iter = {i}, optidx = {opt_idx}, optimal error = {opt_err}, accuracy = {(y_pred_opt==y).sum() / len(y)}")
 
 		if coef.sum() == 0:
 			coef = np.ones(n_mdl) / n_mdl
