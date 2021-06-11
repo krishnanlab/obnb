8194fa1ce71351242aee1a424b4c049716bc4760
Remy <liurenmi@msu.edu>
2021-06-11 11:20:44 -0400

    Fix BioGRID file name

--- get_data.sh
+++ get_data.sh
@@ -18,7 +18,7 @@ data_path=/mnt/research/compbio/krishnanlab/data
 network_path=$data_path/networks
 
 ln -s $network_path/string/v10/String_experiments.edg data/networks/STRING-EXP.edg
-ln -s $network_path/BioGRID/BioGrid_3.4.136.edgelist data/networks/BioGRID
+ln -s $network_path/BioGRID/BioGrid_3.4.136.edgelist data/networks/BioGRID_3.4.136.edg
 
 ln -s $data_path/functional_annotations/kegg/c2.cp.kegg.v6.1.entrez.BP.gsea-min10-max200-ovlppt7-jacpt5.nonred.gmt data/labels/KEGGBP.gmt
 ln -s $data_path/disease-gene_annotations/disgenet/disgenet_disease-genes_prop.gsea-min10-max600-ovlppt8-jacpt5.nonred.gmt data/labels/DisGeNet.gmt
