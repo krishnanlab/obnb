4512a280f19f155625d5392658c617731bd004ce
Renming Liu <liurenmi@msu.edu>
2021-08-16 09:33:53 -0700

    Update test

--- src/test_runs/test_negsel.py
+++ src/test_runs/test_negsel.py
@@ -1,19 +1,19 @@
 from sys import path
-path.append('../src/')
+path.append('../')
 from NLEval.label import LabelsetCollection, Filter
 
 # specify p-val threshold
 p_thresh = 0.05
 
 # construct LabelsetCollection object from KEGGBP.gmt
-lsc_orig = LabelsetCollection.SplitLSC.from_gmt('../data/labels/KEGGBP.gmt')
+lsc_orig = LabelsetCollection.SplitLSC.from_gmt('../../data/labels/KEGGBP.gmt')
 # make a copy for comparison, `apply` is inplace
 lsc = lsc_orig.copy()
 # create filer
 f = Filter.NegativeFilterHypergeom(p_thresh)
 
 # apply negative selection filter
-lsc.apply(f)
+lsc.apply(f, inplace=True)
 
 print("p-val threshold = %.2f" % p_thresh)
 # compare size of negative before and after filter. Size should shrink after
