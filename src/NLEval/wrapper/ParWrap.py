87e84597f07c470a2e7945f38bec1371100713d6
Renming Liu <liurenmi@msu.edu>
2021-08-15 18:20:08 -0700

    Return original function for n_jobs = 1, added more tests

--- src/NLEval/wrapper/ParWrap.py
+++ src/NLEval/wrapper/ParWrap.py
@@ -7,39 +7,43 @@ class ParDat:
 		self.n_jobs = n_jobs
 		self.verbose = verbose
 
-	def __call__(self, fun):
+	def __call__(self, func):
 		def wrapper(**kwargs):
 			def worker(conn, q):
 				worker_id = conn.recv()
 				job_id = worker_id
 				while job_id != None:
-					result = fun(self.iter_obj[job_id], **kwargs)
+					result = func(self.iter_obj[job_id], **kwargs)
 					q.put((worker_id, result))
 					job_id = conn.recv()
 				conn.close()
 
 			#n_jobs = checkers.checkWorkers(self.n_jobs, len(self.iter_obj))
 			n_jobs = self.n_jobs
-			q = mp.Queue()
-			p = {}
-			PrConn = {}
+			if n_jobs > 1:
+				q = mp.Queue()
+				p = {}
+				PrConn = {}
 
-			for job_id in range(len(self.iter_obj)):
-				if len(p) < n_jobs:
-					PrConn[job_id], ChConn = mp.Pipe()
-					p[job_id] = mp.Process(target=worker, args=(ChConn, q))
-					p[job_id].daemon = True
-					p[job_id].start()
-					PrConn[job_id].send(job_id)
-				else:
+				for job_id in range(len(self.iter_obj)):
+					if len(p) < n_jobs:
+						PrConn[job_id], ChConn = mp.Pipe()
+						p[job_id] = mp.Process(target=worker, args=(ChConn, q))
+						p[job_id].daemon = True
+						p[job_id].start()
+						PrConn[job_id].send(job_id)
+					else:
+						worker_id, result = q.get()
+						PrConn[worker_id].send(job_id)
+						yield result
+				for _ in p:
 					worker_id, result = q.get()
-					PrConn[worker_id].send(job_id)
+					PrConn[worker_id].send(None)
+					p[worker_id].join()
 					yield result
-			for worker in p:
-				worker_id, result = q.get()
-				PrConn[worker_id].send(None)
-				p[worker_id].join()
-				yield result
+			else:
+				for job in self.iter_obj:
+					yield func(job, **kwargs)
 		return wrapper
 
 	@property
--- src/test_runs/test_ParDat.py
+++ src/test_runs/test_ParDat.py
@@ -1,15 +1,27 @@
 from sys import path
 path.append("../")
-from NLEval.wrapper import ParWrap
+from NLEval.wrapper.ParWrap import ParDat
 import time
 
-lst = list(range(20))
-@ParWrap.ParDat(lst)
-def test(a,b,c):
-	time.sleep(2)
-	#print("a=%s"%repr(a))
-	return a
+lst = list(range(10))
 
-for i in test(b=3,c=5):
-	print(i)
+@ParDat(lst, n_jobs=5)
+def test5(a,b,c):
+	time.sleep(2)
+	return a, b, c
+
+@ParDat(lst, n_jobs=12)
+def test12(a,b,c):
+	time.sleep(2)
+	return a, b, c
+
+@ParDat(lst, n_jobs=1)
+def test1(a,b,c):
+	time.sleep(2)
+	return a, b, c
+
+for test in test5, test12, test1:
+	for i in test(b=3,c=5):
+		print(i)
+	print('')
 
