1ad5d0fcf50894b677bb340b083e839c7f385f44
Remy <liurenmi@msu.edu>
2021-11-28 17:38:26 -0500

    Rename node_ID to node_id

--- src/NLEval/valsplit/Holdout.py
+++ src/NLEval/valsplit/Holdout.py
@@ -64,7 +64,7 @@ class TrainValTest(BaseHoldout):
         sorted_node_ids = sorted(
             common_ids,
             reverse=self.train_on == "bot",
-            key=lambda node_ID: lsc_ids.getProp(node_ID, prop_name),
+            key=lambda node_id: lsc_ids.getProp(node_id, prop_name),
         )
 
         n = len(sorted_node_ids)
@@ -117,7 +117,7 @@ class BinHold(BaseHoldout):
         sorted_node_ids = sorted(
             common_ids,
             reverse=self.train_on == "bot",
-            key=lambda node_ID: lsc_ids.getProp(node_ID, prop_name),
+            key=lambda node_id: lsc_ids.getProp(node_id, prop_name),
         )
         bin_size = np.floor(len(sorted_node_ids) / self.bin_num).astype(int)
         self._test_index = np.array(sorted_node_ids[:bin_size])
@@ -163,13 +163,13 @@ class ThreshHold(BaseHoldout):
         lsc_ids._check_prop_existence(prop_name, True)
         top_list = []
         bot_list = []
-        for node_ID in node_ids.lst:
-            if node_ID in lsc_ids:
-                if lsc_ids.getProp(node_ID, "Noccur") > 0:
-                    if lsc_ids.getProp(node_ID, prop_name) >= self.cut_off:
-                        top_list.append(node_ID)
+        for node_id in node_ids.lst:
+            if node_id in lsc_ids:
+                if lsc_ids.getProp(node_id, "Noccur") > 0:
+                    if lsc_ids.getProp(node_id, prop_name) >= self.cut_off:
+                        top_list.append(node_id)
                     else:
-                        bot_list.append(node_ID)
+                        bot_list.append(node_id)
 
         if self.train_on == "top":
             self._train_index, self._test_index = top_list, bot_list
