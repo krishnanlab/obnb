64ddcce42122ef51f6f2c5f6b5fb8241759901e6
Remy <liurenmi@msu.edu>
2021-12-23 17:46:12 -0500

    Update ParDat run test

--- test/run/ParDat.py
+++ test/run/ParDat.py
@@ -5,20 +5,16 @@ from NLEval.wrapper.ParWrap import ParDat
 import time
 
 lst = list(range(10))
-verbose = True
-verb_kws = {"bar_length": 40, "log_steps": 3}
 
-for n_workers in 5, 12, 1, 0:
-    print(f"Start testing n_workers = {n_workers}")
+for verbose in True, False:
+    for n_workers in 5, 12, 1, 0:
+        print(f"Start testing {n_workers=}, {verbose=}")
 
-    @ParDat(lst, n_workers=n_workers, verbose=verbose, verb_kws=verb_kws)
-    def test(a, b, c):
-        time.sleep(1)
-        return a, b, c
+        @ParDat(lst, n_workers=n_workers, verbose=verbose, bar_length=40)
+        def test(a, b, c):
+            time.sleep(0.5)
+            return a, b, c
 
-    for i in test(b=3, c=5):
-        if verbose:
-            pass
-        else:
-            print(i)
-    print("")
+        for i in test(b=3, c=5):
+            if not verbose:
+                print(i)
