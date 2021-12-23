2a66758a8ace81fe0ed5c0e1a012efd841d1ce0b
Remy <liurenmi@msu.edu>
2021-12-23 12:19:23 -0500

    Add simple sqrt test case for ParDat

--- src/unit_tests/test_wrapper.py
+++ src/unit_tests/test_wrapper.py
@@ -7,7 +7,7 @@ from sklearn.linear_model import LogisticRegression
 from NLEval.wrapper.ParWrap import ParDat
 
 
-class TestParDat(unittest.TestCase):
+class TestParDatLogReg(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.n_samples = 1000
@@ -46,6 +46,34 @@ class TestParDat(unittest.TestCase):
             n_workers, verbose = param
             with self.subTest(n_workers=n_workers, verbose=verbose):
                 self.parallel_logreg_training(n_workers, verbose)
+
+
+class TestParDat(unittest.TestCase):
+    def setUp(self):
+        self.num_list = num_list = list(range(100))
+        self.sqrt_list = np.sqrt(num_list).tolist()
+        self.job_list = list(range(len(num_list)))
+        self.n_workers = 4
+
+    def test_parallel_sqrt_decoratedfunc(self):
+        @ParDat(job_list=self.job_list, n_workers=self.n_workers)
+        def func(i):
+            return i, np.sqrt(self.num_list[i])
+        out_list = [None for _ in self.job_list]
+        for i, j in func():
+            out_list[i] = j
+
+        self.assertEqual(self.sqrt_list, out_list)
+
+    def test_parallel_sqrt_wrapedfunc(self):
+        def func(i):
+            return i, np.sqrt(self.num_list[i])
+
+        out_list = [None for _ in self.job_list]
+        for i, j in ParDat(job_list=self.job_list, n_workers=self.n_workers)(func)():
+            out_list[i] = j
+
+        self.assertEqual(self.sqrt_list, out_list)
 
 
 if __name__ == "__main__":
