73fafff67b42b3a3bc3e3d39b71371bcccbefdda
Remy <liurenmi@msu.edu>
2021-12-30 14:30:01 -0500

    Deprecate model

--- src/NLEval/model/__init__.py
+++ src/NLEval/model/__init__.py
@@ -1,4 +1,13 @@
+import warnings
+
 from NLEval.model import LabelPropagation
 from NLEval.model import SupervisedLearning
+
+warnings.simplefilter("always", DeprecationWarning)
+warnings.warn(
+    "The module model is deprecated",
+    category=DeprecationWarning,
+    stacklevel=2,
+)
 
 __all__ = ["LabelPropagation", "SupervisedLearning"]
