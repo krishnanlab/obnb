f341377385823af7e2030b755182b9fbdf2a69c7
Remy <liurenmi@msu.edu>
2021-12-31 12:56:31 -0500

    Implement value checkers

--- src/NLEval/util/checkers.py
+++ src/NLEval/util/checkers.py
@@ -21,6 +21,8 @@ __all__ = [
     "NUMERIC_TYPE",
     "NUMSTRING_TYPE",
     "ITERABLE_TYPE",
+    "checkValuePositive",
+    "checkValueNonnegative",
     "checkType",
     "checkTypeErrNone",
     "checkTypeAllowNone",
@@ -40,6 +42,18 @@ FLOAT_TYPE = (float, np.float32, np.float64, np.float128)
 NUMERIC_TYPE = INT_TYPE + FLOAT_TYPE
 NUMSTRING_TYPE = INT_TYPE + FLOAT_TYPE + (str,)
 ITERABLE_TYPE = Iterable
+
+
+def checkValuePositive(name, val):
+    """Check if the input value is positive."""
+    if not val > 0:
+        raise ValueError(f"{name!r} should be positive, got {val}")
+
+
+def checkValueNonnegative(name, val):
+    """Check if the input value is non-negative."""
+    if not val >= 0:
+        raise ValueError(f"{name!r} should be non-negative, got {val}")
 
 
 def checkType(name, targetType, val):
