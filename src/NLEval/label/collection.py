da1b5254f430562e8ce6e3cc38dcd0d12c3e8109
Remy <liurenmi@msu.edu>
2022-01-09 18:47:43 -0500

    Deprecate valsplit and SplitLSC

--- src/NLEval/label/collection.py
+++ src/NLEval/label/collection.py
@@ -9,11 +9,9 @@ from typing import Set
 from typing import Tuple
 
 import numpy as np
-from NLEval.label import filters
 from NLEval.label.filters.base import BaseFilter
 from NLEval.util import checkers
 from NLEval.util import idhandler
-from NLEval.util.deprecated import Deprecated
 from NLEval.util.exceptions import IDExistsError
 
 Splitter = Callable[[np.ndarray, np.ndarray], Iterator[Tuple[np.ndarray, ...]]]
@@ -551,115 +549,3 @@ class LabelsetCollection(idhandler.IDprop):
                 label_id, label_info, *lst = line.strip().split(sep)
                 lsc.add_labelset(lst, label_id, label_info)
         return lsc
-
-
-class SplitLSC(LabelsetCollection):
-    """Labelset collection equipped with split generator."""
-
-    @Deprecated("SplitLSC is deprecated, use labelset_split instead")
-    def __init__(self):
-        """Initialize SplitLSC object."""
-        super().__init__()
-        self._valsplit = None
-
-    @property
-    def valsplit(self):
-        """Ssplit generator for generating train/(val)/test splits."""
-        return self._valsplit
-
-    @valsplit.setter
-    def valsplit(self, obj):
-        self._valsplit = obj
-
-    def train_test_setup(self, graph, prop_name=None, min_pos=10):
-        """Set up training and testing.
-
-        Filter labelsets based on train/test samples
-
-        Args:
-            prop_name(str): name of properties used for generating splits.
-            min_pos(int): minimum number of positive in both training and
-                testing sets of a given labelset below which labelset is
-                discarded. If ``None`` specified, no filtering will be done.
-
-        """
-        if self.valsplit is None:
-            raise AttributeError(
-                "'valsplit' not configured, please assign validation split "
-                "generator first. See `NLEval.valsplit` for more info.",
-            )
-
-        num_labelsets = None
-        while num_labelsets != len(self.label_ids):
-            num_labelsets = len(self.label_ids)
-            # print(num_labelsets)
-            # label_id_set = set(self.label_ids)
-            self.valsplit.train_test_setup(self.entity, graph.idmap, prop_name)
-            self.apply(
-                filters.LabelsetRangeFilterTrainTestPos(min_pos),
-                inplace=True,
-            )
-            # for i in label_id_set - set(self.label_ids):
-            #     print(f"Pop {i}")
-
-    def split_labelset(self, label_id, entity_ids=None):
-        """Split up a labelset by training and testing sets.
-
-        Returns:
-            A generator that yeilds train/test IDs and labels, see
-            `NLEval.valsplit.Base.BaseValSplit.split` for more info.
-
-        """
-        if entity_ids is None:
-            entity_ids = self.entity_ids.copy()
-
-        pos_ids_set = set(self.get_labelset(label_id)) & set(entity_ids)
-        neg_ids_set = set(self.get_negative(label_id)) & set(entity_ids)
-
-        id_ary = np.array(list(pos_ids_set) + list(neg_ids_set))
-        label_ary = np.zeros(len(id_ary), dtype=bool)
-        label_ary[: len(pos_ids_set)] = True
-        return self.valsplit.split(id_ary, label_ary)
-
-    def export_splits(self, fp, graph):
-        """Export (holdout) split information to npz file.
-
-        Notes:
-            * Only allow ``Holdout`` split type for now, since it is not
-                specific for each label
-            * Ignores neutral and set everything not positives as negatives,
-                in the future, add an option to allow neutral labels by
-                setting positive, neutral, and negative as +1, 0, and -1,
-                respectively
-            * Currently not checking whether the validation split is aligned
-                with the graph, in the future, need to think of a way to make
-                sure this is checked
-
-        Args:
-            fp(str): output file path
-            graph(:obj:`NLEval.graph`): graph object, more specifically the IDs
-                of the nodes in the graph, used for filtering IDs
-
-        """
-        valid = False if self.valsplit.valid_index is None else True
-        self.valsplit.check_split_setup(valid)
-
-        train_idx = graph.idmap[self.valsplit.train_index]
-        test_idx = graph.idmap[self.valsplit.test_index]
-        valid_idx = graph.idmap[self.valsplit.valid_index] if valid else np.NaN
-
-        y = np.zeros((graph.size, len(self.label_ids)), dtype=bool)
-        for i, label_id in enumerate(self.label_ids):
-            pos_id_ary = np.array(list(self.get_labelset(label_id)))
-            pos_idx_ary = graph.idmap[pos_id_ary]
-            y[pos_idx_ary, i] = True
-
-        np.savez(
-            fp,
-            y=y,
-            train_idx=train_idx,
-            valid_idx=valid_idx,
-            test_idx=test_idx,
-            IDs=graph.idmap.lst,
-            label_ids=self.label_ids,
-        )
