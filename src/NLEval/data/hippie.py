436ec9ed96d68d233fe76b201e58795895950064
Remy <liurenmi@msu.edu>
2022-01-27 17:04:06 -0500

    Add HIPPIE network

---
+++ src/NLEval/data/hippie.py
@@ -0,0 +1,25 @@
+from .base import BaseNdexData
+
+
+class HIPPIE(BaseNdexData):
+    """The HIPPIE Human scored Protein Protein Interaction network.
+
+    Note: the inferred PPI directionality is disregarded, i.e. the resulting
+        network is undirected.
+
+    """
+
+    cx_uuid = "89dd3925-3718-11e9-9f06-0ac135e8bacf"
+
+    def __init__(self, root: str):
+        """Initialize the HIPPIE network data."""
+        # TODO: edge weights mean reduction when multiple values are available
+        super().__init__(
+            root,
+            weighted=True,
+            directed=False,
+            interaction_types=["interacts-with"],
+            node_id_prefix="ncbigene",
+            default_edge_weight=0.0,
+            edge_weight_attr_name="Confidence Value",
+        )
