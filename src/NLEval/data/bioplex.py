ca8812273d3bfadb92b47f1b32b73fe2e673ebf5
Remy <liurenmi@msu.edu>
2022-01-27 14:52:12 -0500

    Add BioPlex3-shared network

---
+++ src/NLEval/data/bioplex.py
@@ -0,0 +1,18 @@
+from .base import BaseNdexData
+
+
+class BioPlex(BaseNdexData):
+    """The BioPlex3-shared Protein Protein Interaction network."""
+
+    cx_uuid = "f7a218c0-2376-11ea-bb65-0ac135e8bacf"
+
+    def __init__(self, root: str):
+        """Initialize the BioGRID network data."""
+        super().__init__(
+            root,
+            weighted=False,
+            directed=False,
+            interaction_types=["interacts-with"],
+            node_id_prefix="ncbigene",
+            node_id_entry="r",
+        )
