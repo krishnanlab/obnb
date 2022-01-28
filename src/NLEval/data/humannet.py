a2f1a5b58b5fdf050f9c269bb5b6fc4aae953b2b
Remy <liurenmi@msu.edu>
2022-01-27 21:51:32 -0500

    Add HumanNet-NF network

---
+++ src/NLEval/data/humannet.py
@@ -0,0 +1,19 @@
+from .base import BaseNdexData
+
+
+class HumanNet(BaseNdexData):
+    """The HumanNet-FN functional interaction network."""
+
+    cx_uuid = "0d05756c-3553-11e9-9f06-0ac135e8bacf"
+
+    def __init__(self, root: str):
+        """Initialize the HumanNet network data."""
+        super().__init__(
+            root,
+            weighted=True,
+            directed=False,
+            interaction_types=["associates-with"],
+            node_id_prefix="ncbigene",
+            default_edge_weight=0.0,
+            edge_weight_attr_name="LLS",
+        )
