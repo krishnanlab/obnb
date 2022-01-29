0ecec1d6969157de751e5b0bbc8eda9c2a5a7a64
Remy <liurenmi@msu.edu>
2022-01-29 18:21:28 -0500

    Load from processed npz files after the first time

--- src/NLEval/data/base.py
+++ src/NLEval/data/base.py
@@ -26,16 +26,29 @@ class BaseNdexData(SparseGraph):
         super().__init__(weighted=weighted, directed=directed)
 
         self.root = root
-        self._download()
-        self.read_cx_stream_file(self.raw_data_path, **kwargs)
+        if self._download():
+            print("Processing...")
+            self.read_cx_stream_file(self.raw_data_path, **kwargs)
+            print("Done!")
+            self.save_npz(self.processed_data_path, weighted)
+        else:
+            self.read_npz(self.processed_data_path, weighted, directed)
 
     @property
     def raw_data_path(self) -> str:
-        return osp.join(self.raw_dir, "raw_data.cx")
+        return osp.join(self.raw_dir, "data.cx")
+
+    @property
+    def processed_data_path(self) -> str:
+        return osp.join(self.processed_dir, "data.npz")
 
     @property
     def raw_dir(self) -> str:
-        return osp.join(self.root, self.__class__.__name__)
+        return osp.join(self.root, self.__class__.__name__, "raw")
+
+    @property
+    def processed_dir(self) -> str:
+        return osp.join(self.root, self.__class__.__name__, "processed")
 
     def download(self):
         """Download data from NDEX via ndex2 client."""
@@ -44,11 +57,20 @@ class BaseNdexData(SparseGraph):
         with open(self.raw_data_path, "wb") as f:
             f.write(client_resp.content)
 
-    def _download(self):
-        """Check to see if files downloaded first before downloading."""
+    def _download(self) -> bool:
+        """Check to see if files downloaded first before downloading.
+
+        Returns:
+            bool: False if already downloaded, otherwise True.
+
+        """
         if not osp.isdir(self.raw_dir):
             os.makedirs(osp.expanduser(osp.normpath(self.raw_dir)))
+            os.makedirs(osp.expanduser(osp.normpath(self.processed_dir)))
+
         if not osp.isfile(self.raw_data_path):
             print("Downloading...")
             self.download()
-            print("Done!")
+            return True
+
+        return False
