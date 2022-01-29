4d5a7a9c9bbd398f0f8c378b984e3d068c3f9b05
Remy <liurenmi@msu.edu>
2022-01-29 09:37:48 -0500

    Add data classes to __init__

--- src/NLEval/data/__init__.py
+++ src/NLEval/data/__init__.py
@@ -1,1 +1,14 @@
 """Interface with various databases to retrieve data."""
+from .biogrid import BioGRID
+from .bioplex import BioPlex
+from .hippie import HIPPIE
+from .humannet import HumanNet
+from .string import STRING
+
+__all__ = [
+    "BioGRID",
+    "BioPlex",
+    "HIPPIE",
+    "HumanNet",
+    "STRING",
+]
