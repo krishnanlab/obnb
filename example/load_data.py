e6fd174978f3764c1d5c645f0feb6d4e3dd23bf4
Remy <liurenmi@msu.edu>
2022-02-06 10:20:02 -0500

    Add option for loading sparse graph and skip negative filtering

--- example/load_data.py
+++ example/load_data.py
@@ -2,11 +2,28 @@ import os.path as osp
 
 import numpy as np
 from NLEval.graph import DenseGraph
+from NLEval.graph import SparseGraph
 from NLEval.label import filters
 from NLEval.label import LabelsetCollection
 
 
-def load_data(network="STRING-EXP", label="KEGGBP"):
+def load_data(
+    network: str = "STRING-EXP",
+    label: str = "KEGGBP",
+    sparse: bool = False,
+    filter_negative: bool = True,
+):
+    """Load graph and node labels.
+
+    Args:
+        network (str): Name of the network to use (default: "STRING-EXP").
+        label (str): Name of the geneset collection to use (default: "KEGGBP").
+        sparse (bool): Whether or not to load the network as sparse graph
+            instead of dense graph (default: :obj:`False`).
+        filter_negative (bool): Whether or not to filter negative genes based
+            on hypergeometric test (default: :obj:`True`).
+
+    """
     data_dir = osp.join(osp.pardir, "data")
     graph_path = osp.join(data_dir, "networks", f"{network}.edg")
     label_path = osp.join(data_dir, "labels", f"{label}.gmt")
@@ -15,14 +32,16 @@ def load_data(network="STRING-EXP", label="KEGGBP"):
     print(f"{network=}\n{label=}")
 
     # Load data
-    g = DenseGraph.from_edglst(graph_path, weighted=True, directed=False)
+    graph_factory = SparseGraph if sparse else DenseGraph
+    g = graph_factory.from_edglst(graph_path, weighted=True, directed=False)
     lsc = LabelsetCollection.from_gmt(label_path)
 
     # Filter labels
     print(f"Number of labelsets before filtering: {len(lsc.label_ids)}")
     lsc.iapply(filters.EntityExistenceFilter(g.idmap.lst))
     lsc.iapply(filters.LabelsetRangeFilterSize(min_val=50))
-    lsc.iapply(filters.NegativeGeneratorHypergeom(p_thresh=0.05))
+    if filter_negative:
+        lsc.iapply(filters.NegativeGeneratorHypergeom(p_thresh=0.05))
     print(f"Number of labelsets after filtering: {len(lsc.label_ids)}")
 
     # Load gene properties for study-bias holdout
