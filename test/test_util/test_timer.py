5772a07749cf1389b48c0f5b326d089eb532ab63
Remy <liurenmi@msu.edu>
2022-03-11 16:41:17 -0500

    Add timeout decorator

---
+++ test/test_util/test_timer.py
@@ -0,0 +1,18 @@
+import unittest
+from time import sleep
+
+from NLEval.util import timer
+
+
+class TestTimer(unittest.TestCase):
+    def test_timeout_success(self):
+        with timer.Timeout(2):
+            sleep(1)
+
+    def test_timeout_failure(self):
+        with timer.Timeout(1):
+            self.assertRaises(TimeoutError, sleep, 2)
+
+
+if __name__ == "__main__":
+    unittest.main()
