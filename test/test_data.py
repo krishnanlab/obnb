76916ef445550f49fcddac87c5ae8903dab2df3d
Remy <liurenmi@msu.edu>
2022-03-12 10:24:42 -0500

    Clean disc

--- test/test_data.py
+++ test/test_data.py
@@ -1,4 +1,5 @@
-import io
+import gc
+import os
 import shutil
 import tempfile
 import unittest
@@ -20,30 +21,51 @@ full_data_test_param = [
 class TestData(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
+        # Clean after every test function call
         cls.tmp_dir = tempfile.mkdtemp()
-        print(f"Created temporary directory for testing data: {cls.tmp_dir}")
+        # Clean at the end of the test class
+        cls.tmp_dir_preserve = tempfile.mkdtemp()
+        print(
+            f"Created temporary directory for testing data: {cls.tmp_dir}, "
+            f"{cls.tmp_dir_preserve}",
+        )
 
     @classmethod
     def tearDownClass(cls):
         shutil.rmtree(cls.tmp_dir)
-        print(f"Removed temporary directory: {cls.tmp_dir}")
+        shutil.rmtree(cls.tmp_dir_preserve)
+        print(
+            f"Removed temporary directories: {cls.tmp_dir}, "
+            f"{cls.tmp_dir_preserve}",
+        )
+
+    def setUp(self):
+        self.graph = self.lsc = None
+
+    def tearDown(self):
+        for f in os.listdir(self.tmp_dir):
+            path = os.path.join(self.tmp_dir, f)
+            shutil.rmtree(path)
+            print(f"Removed {path}")
+        del self.graph, self.lsc
+        gc.collect()
 
     @pytest.mark.longruns
     def test_biogrid(self):
-        graph = NLEval.data.BioGRID(self.tmp_dir)
-        self.assertEqual(graph.size, 25711)
-        self.assertEqual(graph.num_edges, 1200394)
+        self.graph = NLEval.data.BioGRID(self.tmp_dir)
+        self.assertEqual(self.graph.size, 25711)
+        self.assertEqual(self.graph.num_edges, 1200394)
 
     @parameterized.expand(full_data_test_param)
     def test_bioplex(self, name, reprocess, redownload):
         with self.subTest(name):
-            graph = NLEval.data.BioPlex(
-                self.tmp_dir,
+            self.graph = NLEval.data.BioPlex(
+                self.tmp_dir_preserve,
                 reprocess=reprocess,
                 redownload=redownload,
             )
-            self.assertEqual(graph.size, 8364)
-            self.assertEqual(graph.num_edges, 71408)
+            self.assertEqual(self.graph.size, 8364)
+            self.assertEqual(self.graph.num_edges, 71408)
 
     @pytest.mark.xfail(
         raises=TimeoutError,
@@ -52,37 +74,37 @@ class TestData(unittest.TestCase):
     @pytest.mark.longruns
     def test_disgenet(self):
         with Timeout(600):
-            lsc = NLEval.data.DisGeNet(self.tmp_dir)
+            self.lsc = NLEval.data.DisGeNet(self.tmp_dir)
 
     @pytest.mark.longruns
     def test_funcoup(self):
-        graph = NLEval.data.FunCoup(self.tmp_dir)
-        self.assertEqual(graph.size, 17783)
-        self.assertEqual(graph.num_edges, 10027588)
+        self.graph = NLEval.data.FunCoup(self.tmp_dir)
+        self.assertEqual(self.graph.size, 17783)
+        self.assertEqual(self.graph.num_edges, 10027588)
 
     @parameterized.expand([("GOBP",), ("GOCC",), ("GOMF",)])
     @pytest.mark.longruns
     def test_go(self, name):
         with self.subTest(name):
-            lsc = getattr(NLEval.data, name)(self.tmp_dir)
+            self.lsc = getattr(NLEval.data, name)(self.tmp_dir)
 
     @pytest.mark.longruns
     def test_hippie(self):
-        graph = NLEval.data.HIPPIE(self.tmp_dir)
-        self.assertEqual(graph.size, 17955)
-        self.assertEqual(graph.num_edges, 770754)
+        self.graph = NLEval.data.HIPPIE(self.tmp_dir)
+        self.assertEqual(self.graph.size, 17955)
+        self.assertEqual(self.graph.num_edges, 770754)
 
     @pytest.mark.longruns
     def test_humannet(self):
-        graph = NLEval.data.HumanNet(self.tmp_dir)
-        self.assertEqual(graph.size, 17787)
-        self.assertEqual(graph.num_edges, 849002)
+        self.graph = NLEval.data.HumanNet(self.tmp_dir)
+        self.assertEqual(self.graph.size, 17787)
+        self.assertEqual(self.graph.num_edges, 849002)
 
     @pytest.mark.longruns
     def test_string(self):
-        graph = NLEval.data.STRING(self.tmp_dir)
-        self.assertEqual(graph.size, 18513)
-        self.assertEqual(graph.num_edges, 11038228)
+        self.graph = NLEval.data.STRING(self.tmp_dir)
+        self.assertEqual(self.graph.size, 18513)
+        self.assertEqual(self.graph.num_edges, 11038228)
 
 
 if __name__ == "__main__":
