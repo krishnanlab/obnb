77146925397dce6420867a68e35f268e341857cf
Remy <liurenmi@msu.edu>
2022-04-25 08:43:13 -0400

    Create PCNet (v1.3)

--- src/NLEval/data/network/__init__.py
+++ src/NLEval/data/network/__init__.py
@@ -4,6 +4,7 @@ from .bioplex import BioPlex
 from .funcoup import FunCoup
 from .hippie import HIPPIE
 from .humannet import HumanNet
+from .pcnet import PCNet
 from .string import STRING
 
 __all__ = [
@@ -12,5 +13,6 @@ __all__ = [
     "FunCoup",
     "HIPPIE",
     "HumanNet",
+    "PCNet",
     "STRING",
 ]
---
+++ src/NLEval/data/network/pcnet.py
@@ -0,0 +1,22 @@
+from .base import BaseNdexData
+
+
+class PCNet(BaseNdexData):
+    """The PCNet (v1.3) Parsimonious Composit human gene interaction network."""
+
+    cx_uuid = "7a686aa6-c494-11ec-b397-0ac135e8bacf"
+
+    def __init__(self, root: str, **kwargs):
+        """Initialize the PCNet network data."""
+        super().__init__(
+            root,
+            weighted=False,
+            directed=False,
+            largest_comp=True,
+            cx_kwargs={
+                "interaction_types": ["neighbor-of"],
+                "node_id_entry": "n",
+                "node_id_prefix": None,
+            },
+            **kwargs,
+        )
