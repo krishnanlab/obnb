621bef06268a7c2108cf447dd9a915687d917bb1
Remy Liu <36778645+RemyLau@users.noreply.github.com>
2022-04-26 07:46:42 -0400

    Remove genes with ambiguous Entrez mapping  (#183)
    * Add logger to converter, add option to remove genes with multiple entrez ids
    
    * Update network test stats (removed genes with multiple entrez ids)
    
    * Fix type
    
    * Warn if overwrite
    
    * Set id coverter log level

--- src/NLEval/util/converter.py
+++ src/NLEval/util/converter.py
@@ -2,33 +2,49 @@ import mygene
 
 from ..typing import Dict
 from ..typing import List
+from ..typing import LogLevel
+from ..typing import Optional
+from ..util.logger import get_logger
 
 
 class MyGeneInfoConverter:
     """Gene ID conversion via MyGeneInfo."""
 
-    def __init__(self, species: str = "human", **query_kwargs):
+    def __init__(
+        self,
+        log_level: LogLevel = "INFO",
+        remove_multimap: bool = True,
+        species: str = "human",
+        **query_kwargs,
+    ):
         """Initialize the converter.
 
         Args:
+            log_level: Logging level.
+            remove_multimap (bool): If set to True, then ignore any gene
+                that is mapped to multiple entrez gene id.
             species (str): comma-separated species name of interest
             query_kwargs: Other keyword arguments for
                 mygene.MyGeneInfo.querymany
 
         """
         self.client = mygene.MyGeneInfo()
-        self.convert_map: Dict[str, str] = {}
+        self.convert_map: Dict[str, Optional[str]] = {}
+
+        self.logger = get_logger(self.__class__.__name__, log_level=log_level)
+
+        self.remove_multimap = remove_multimap
         self.species = species
         self.query_kwargs = query_kwargs
 
-    def __call__(self, old_id: str) -> str:
+    def __call__(self, old_id: str) -> Optional[str]:
         """Convert an ID to entrez gene ID.
 
         Args:
             old_id (str): gene/protein ID to be converted.
 
         Returns:
-            str: Entrez gene ID.
+            str: Entrez gene ID, or None if not available.
 
         """
         try:
@@ -54,4 +70,18 @@ class MyGeneInfoConverter:
         )
         for query in queries:
             gene = query["query"]
-            self.convert_map[gene] = query.get("entrezgene")
+            gene_id = query.get("entrezgene")
+            if gene in self.convert_map:
+                if self.remove_multimap:
+                    self.convert_map[gene] = None
+                    self.logger.info(
+                        f"Removing {gene} due to multiple entrez mapping.",
+                    )
+                    continue
+
+                old_gene_id = self.convert_map[gene]
+                self.logger.warning(
+                    f"Overwriting {gene} -> {old_gene_id} to "
+                    f"{gene} -> {gene_id}",
+                )
+            self.convert_map[gene] = gene_id
