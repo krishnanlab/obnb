bd8f3f89c186f35a3ee987fe7f56e71aeb6aa50f
RemyLau <remylau961@gmail.com>
2022-08-26 16:46:00 -0400

    create config module and add base download URLs

---
+++ src/NLEval/_config/config.py
@@ -0,0 +1,14 @@
+"""Configurations and common variables used by NLEval."""
+from NLEval.typing import Dict
+
+__all__ = [
+    "NLEDATA_URL_DICT",
+    "NLEDATA_URL_DICT_DEV",
+    "NLEDATA_URL_DICT_STABLE",
+]
+
+NLEDATA_URL_DICT_STABLE: Dict[str, str] = {}
+NLEDATA_URL_DICT_DEV: Dict[str, str] = {
+    "nledata-v1.0-test": "https://sandbox.zenodo.org/record/1096827/files/",
+}
+NLEDATA_URL_DICT: Dict[str, str] = {**NLEDATA_URL_DICT_STABLE, **NLEDATA_URL_DICT_DEV}
