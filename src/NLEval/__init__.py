a6ed91cba66c524d835623540a59f368524978fa
RemyLau <remylau961@gmail.com>
2022-09-01 15:07:05 -0400

    use dict config for logger

--- src/NLEval/__init__.py
+++ src/NLEval/__init__.py
@@ -1,8 +1,6 @@
 """Collection of network learning evaluation utilities."""
-import logging
-
 from NLEval import graph, label, model_trainer
-from NLEval.util.logger import config_logger
+from NLEval.util.logger import config_logger, get_logger
 
 __version__ = "0.1.0-dev1"
 __data_version__ = "nledata-v0.1.0-dev"
@@ -10,5 +8,4 @@ __all__ = ["graph", "label", "model_trainer"]
 
 # Configure logger setting and create global logger
 config_logger()
-logger = logging.getLogger("NLEval_brief")
-logger.setLevel(logging.INFO)
+logger = get_logger(None, "NLEval", log_level="INFO")
