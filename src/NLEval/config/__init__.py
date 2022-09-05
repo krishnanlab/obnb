70a37aafbb397695a581deaddda83770961244a1
Remy <liurenmi@msu.edu>
2022-09-05 13:36:04 -0400

    bump data version nledata-v0.1.0-dev -> nledata-v0.1.0-dev1

--- src/NLEval/config/__init__.py
+++ src/NLEval/config/__init__.py
@@ -11,5 +11,6 @@ NLEDATA_URL_DICT_STABLE: Dict[str, str] = {}
 NLEDATA_URL_DICT_DEV: Dict[str, str] = {
     "nledata-v1.0-test": "https://sandbox.zenodo.org/record/1096827/files/",
     "nledata-v0.1.0-dev": "https://sandbox.zenodo.org/record/1097545/files/",
+    "nledata-v0.1.0-dev1": "https://sandbox.zenodo.org/record/1099982/files/",
 }
 NLEDATA_URL_DICT: Dict[str, str] = {**NLEDATA_URL_DICT_STABLE, **NLEDATA_URL_DICT_DEV}
