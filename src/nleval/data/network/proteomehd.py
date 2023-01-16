ca46f503f1fbe544b47a01ca18523f7514857350
Remy <liurenmi@msu.edu>
2023-01-16 16:11:38 -0500

    use top 0.5% ProteomHD interactions as unweighted graph by default

--- src/nleval/data/network/proteomehd.py
+++ src/nleval/data/network/proteomehd.py
@@ -5,13 +5,13 @@ from nleval.typing import Converter
 class ProteomeHD(BaseNDExData):
     """The ProteomeHD Protein Protein Interaction network."""
 
-    cx_uuid = "0dc757e1-939c-11e9-bcaf-0ac135e8bacf"
+    cx_uuid = "4cb4b0f3-83da-11e9-848d-0ac135e8bacf"
 
     def __init__(
         self,
         root: str,
         *,
-        weighted: bool = True,
+        weighted: bool = False,
         directed: bool = False,
         largest_comp: bool = True,
         gene_id_converter: Converter = "HumanEntrez",
@@ -27,7 +27,7 @@ class ProteomeHD(BaseNDExData):
             cx_kwargs={
                 "interaction_types": ["correlates-with"],
                 "node_id_prefix": "ncbigene",
-                "default_edge_weight": 0.0,
+                "default_edge_weight": 1.0,
                 "edge_weight_attr_name": "score",
                 "use_node_alias": True,
             },
