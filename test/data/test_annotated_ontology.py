f3ad17e6e5469d0ae32f2739310a8562fa682e5f
Remy <liurenmi@msu.edu>
2023-02-11 11:06:41 -0500

    add test for new annotated ontology data objects

--- test/data/test_annotated_ontology.py
+++ test/data/test_annotated_ontology.py
@@ -5,12 +5,13 @@ import yaml
 
 import nleval
 from nleval.data.annotated_ontology.disgenet import DisGeNET
+from nleval.data.annotated_ontology.go import GO, GOBP, GOCC, GOMF
 from nleval.label.filters import LabelsetRangeFilterSize
 from nleval.util.path import hexdigest
 
 
 @pytest.mark.mediumruns
-def test_disgenet(tmpdir, caplog, mocker, subtests):
+def test_disgenet(tmpdir, mocker, subtests):
     datadir = osp.join(tmpdir, "DisGeNET")
     filter_ = LabelsetRangeFilterSize(min_val=100, max_val=200)
     hexhash = hexdigest(yaml.dump(filter_.to_config()))
@@ -22,8 +23,11 @@ def test_disgenet(tmpdir, caplog, mocker, subtests):
     transform_called = 0
 
     with subtests.test("Normal download"):
-        DisGeNET(tmpdir)
+        lsc = DisGeNET(tmpdir)
+        assert len(lsc.label_ids) > 0
         assert osp.isdir(datadir)
+        assert osp.isdir(osp.join(tmpdir, "DisGeNETAnnotation"))
+        assert osp.isdir(osp.join(tmpdir, "MondoDiseaseOntology"))
         assert osp.isdir(osp.join(datadir, "processed"))
         assert osp.isdir(osp.join(datadir, "raw"))
         assert osp.isdir(osp.join(datadir, "info"))
@@ -58,3 +62,33 @@ def test_disgenet(tmpdir, caplog, mocker, subtests):
     # with subtests.test("Cannot set pre-transform for archived data"):
     #     with pytest.raises(ValueError):
     #         DisGeNET(tmpdir, pre_transform=filter_, version="nledata-v1.0-test")
+
+
+@pytest.mark.mediumruns
+def test_go(tmpdir, mocker, subtests):
+    with subtests.test("GO"):
+        lsc = GO(tmpdir)
+        assert len(lsc.label_ids) > 0
+
+        assert osp.isdir(osp.join(tmpdir, "GO"))
+        assert osp.isdir(osp.join(tmpdir, "GO", "processed"))
+        assert osp.isdir(osp.join(tmpdir, "GO", "raw"))
+        assert osp.isdir(osp.join(tmpdir, "GO", "info"))
+
+        assert osp.isdir(osp.join(tmpdir, "GeneOntologyAnnotation"))
+        assert osp.isdir(osp.join(tmpdir, "GeneOntology"))
+
+    with subtests.test("GO"):
+        lsc = GOBP(tmpdir)
+        assert len(lsc.label_ids) > 0
+        assert osp.isdir(osp.join(tmpdir, "GOBP"))
+
+    with subtests.test("GO"):
+        lsc = GOCC(tmpdir)
+        assert len(lsc.label_ids) > 0
+        assert osp.isdir(osp.join(tmpdir, "GOCC"))
+
+    with subtests.test("GO"):
+        lsc = GOMF(tmpdir)
+        assert len(lsc.label_ids) > 0
+        assert osp.isdir(osp.join(tmpdir, "GOMF"))
