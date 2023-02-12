18d46051d692bf56c83cc673742b0d181bbe9698
Remy <liurenmi@msu.edu>
2023-02-12 11:16:26 -0500

    implement HPO gene set collection data object

--- src/nleval/data/annotated_ontology/__init__.py
+++ src/nleval/data/annotated_ontology/__init__.py
@@ -2,6 +2,7 @@
 from nleval.data.annotated_ontology.diseases import DISEASES
 from nleval.data.annotated_ontology.disgenet import DisGeNET
 from nleval.data.annotated_ontology.go import GOBP, GOCC, GOMF
+from nleval.data.annotated_ontology.hpo import HPO
 
 __all__ = [
     "DISEASES",
@@ -9,4 +10,5 @@ __all__ = [
     "GOBP",
     "GOCC",
     "GOMF",
+    "HPO",
 ]
---
+++ src/nleval/data/annotated_ontology/hpo.py
@@ -0,0 +1,52 @@
+from nleval.data.annotated_ontology.base import BaseAnnotatedOntologyData
+from nleval.data.annotation import HumanPhenotypeOntologyAnnotation
+from nleval.data.ontology import MondoDiseaseOntology
+from nleval.label.filters import Compose, LabelsetNonRedFilter, LabelsetRangeFilterSize
+from nleval.typing import List, Mapping, Optional, Union
+
+
+class HPO(BaseAnnotatedOntologyData):
+    """The HPO disease and triat gene set collection."""
+
+    def __init__(
+        self,
+        root: str,
+        min_size: int = 10,
+        max_size: int = 600,
+        overlap: float = 0.7,
+        jaccard: float = 0.5,
+        data_sources: Optional[List[str]] = None,
+        gene_id_converter: Optional[Union[Mapping[str, str], str]] = None,
+        redownload: bool = False,
+        **kwargs,
+    ):
+        """Initialize the HPO data object."""
+        self.min_size = min_size
+        self.max_size = max_size
+        self.jaccard = jaccard
+        self.overlap = overlap
+
+        annotation = HumanPhenotypeOntologyAnnotation(
+            root,
+            data_sources=data_sources,
+            gene_id_converter=gene_id_converter,
+            redownload=redownload,
+        )
+        ontology = MondoDiseaseOntology(root, xref_prefix="HP", redownload=redownload)
+
+        super().__init__(
+            root,
+            annotation=annotation,
+            ontology=ontology,
+            redownload=redownload,
+            **kwargs,
+        )
+
+    @property
+    def default_pre_transform(self):
+        return Compose(
+            LabelsetRangeFilterSize(max_val=self.max_size),
+            LabelsetNonRedFilter(self.jaccard, self.overlap),
+            LabelsetRangeFilterSize(min_val=self.min_size),
+            log_level=self.log_level,
+        )
