d136c735bd85d9072943f526afb7c0a81420733f
Remy <liurenmi@msu.edu>
2023-03-12 11:19:47 -0400

    refactor release_data.py

--- script/release_data.py
+++ script/release_data.py
@@ -6,64 +6,79 @@ from shutil import make_archive, rmtree
 import nleval
 import nleval.data
 import nleval.data.annotation
+from nleval import logger
 from nleval.config import NLEDATA_URL_DICT
 from nleval.data.base import BaseData
 from nleval.util.converter import GenePropertyConverter
 
-homedir = Path(".").resolve()
-datadir = osp.join(homedir, "data_release")
-archdir = osp.join(datadir, "archived")
+HOMEDIR = Path(__file__).resolve().parent
+DATADIR = HOMEDIR / "data_release"
+ARCHDIR = DATADIR / "archived"
 
-all_data = sorted(nleval.data.__all__)
-annotation_data = sorted(nleval.data.annotation.__all__)
-new_data_release = nleval.__data_version__
+ALL_DATA = sorted(nleval.data.__all__)
+ANNOTATION_DATA = sorted(nleval.data.annotation.__all__)
+DATA_RELEASE_VERSION = nleval.__data_version__
 
-if (url := NLEDATA_URL_DICT.get(new_data_release)) is not None:
-    raise ValueError(f"Data release version {new_data_release} exists ({url})")
 
-# Set this to enable setting the correct version number instead of 'latest'
-BaseData._new_data_release = new_data_release
+def setup_version():
+    if (url := NLEDATA_URL_DICT.get(DATA_RELEASE_VERSION)) is not None:
+        raise ValueError(f"Data release version {DATA_RELEASE_VERSION} exists ({url})")
 
-logger = nleval.logger
-logger.info(f"{homedir=!r}")
-logger.info(
-    f"Processing {len(all_data)} data objects for release "
-    f"{new_data_release!r}:\n{pformat(all_data)}",
-)
+    # Set this to enable setting the correct version number instead of 'latest'
+    BaseData._new_data_release = DATA_RELEASE_VERSION
 
-# Clean up old data
-while osp.isdir(datadir):
-    # TODO: make --allow-dirty option
-    answer = input(f"Release data dir already exists ({datadir}), remove now? [yes/no]")
-    if answer == "yes":
-        logger.info(f"Removing old archives in {datadir}")
-        rmtree(datadir)
-        break
-    elif answer == "no":
-        exit()
-    else:
-        logger.error(f"Unknown option {answer!r}, please answer 'yes' or 'no'")
+    logger.info(f"{HOMEDIR=!r}")
+    logger.info(
+        f"Processing {len(ALL_DATA)} data objects for release "
+        f"{DATA_RELEASE_VERSION!r}:\n{pformat(ALL_DATA)}",
+    )
 
-# Download, process, and archive all data
-for name in all_data:
-    getattr(nleval.data, name)(datadir)
-    if name in annotation_data:
-        # NOTE: annotation data objects could contain multiple raw files
-        # preprared by different annotated ontology objects, so we need to wait
-        # until all annotations are prepared before archiving them.
-        continue
-    # TODO: validate data and print stats (#nodes&#edges for networks; stats() for lsc)
-    make_archive(osp.join(archdir, name), "zip", datadir, name, logger=logger)
 
-# Archive annotation data once all raw files are prepared
-for name in annotation_data:
-    make_archive(osp.join(archdir, name), "zip", datadir, name, logger=logger)
+def setup_dir():
+    # Clean up old data
+    while osp.isdir(DATADIR):
+        # TODO: make --allow-dirty option
+        answer = input(f"Release data dir exists ({DATADIR}), remove now? [yes/no]")
+        if answer == "yes":
+            logger.info(f"Removing old archives in {DATADIR}")
+            rmtree(DATADIR)
+            break
+        elif answer == "no":
+            exit()
+        else:
+            logger.error(f"Unknown option {answer!r}, please answer 'yes' or 'no'")
 
-# Download and process gene property data
-GenePropertyConverter(datadir, name="PubMedCount")
 
-# Archive cache
-make_archive(osp.join(archdir, ".cache"), "zip", datadir, ".cache", logger=logger)
+def download_process():
+    # Download, process, and archive all data
+    for name in ALL_DATA:
+        getattr(nleval.data, name)(DATADIR)
+        if name in ANNOTATION_DATA:
+            # NOTE: annotation data objects could contain multiple raw files
+            # prepared by different annotated ontology objects, so we need to
+            # wait until all annotations are prepared before archiving them.
+            continue
+        # TODO: validate data and print stats (#nodes&#edges for nets; stats() for lsc)
+        make_archive(osp.join(ARCHDIR, name), "zip", DATADIR, name, logger=logger)
 
-# TODO: validation summaries -> # of datasets, with one of them failed/succeeded
-# TODO: optionally, upload to zenodo and validate once done (check failed uploads)
+    # Archive annotation data once all raw files are prepared
+    for name in ANNOTATION_DATA:
+        make_archive(osp.join(ARCHDIR, name), "zip", DATADIR, name, logger=logger)
+
+    # Download and process gene property data
+    GenePropertyConverter(DATADIR, name="PubMedCount")
+
+    # Archive cache
+    make_archive(osp.join(ARCHDIR, ".cache"), "zip", DATADIR, ".cache", logger=logger)
+
+
+def main():
+    setup_version()
+    setup_dir()
+    download_process()
+    # TODO: validation summaries -> # of datasets, with one of them failed/succeeded
+    # TODO: optionally, upload to zenodo and validate once done (check failed uploads)
+
+
+if __name__ == "__main__":
+    main()
