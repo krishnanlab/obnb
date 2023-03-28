495299f1af0a64dc2916dd84e6ab9ce6cc0c54a6
Remy <liurenmi@msu.edu>
2023-03-28 18:13:54 -0400

    upgrade pytorch 2.0 and pyg 2.3

--- install.sh
+++ install.sh
@@ -4,33 +4,33 @@
 # as PyTorch and Pytorch Geometric (PYG).
 #
 # Example:
-# $ source install.sh cu102  # install nle with CUDA 10.2
+# $ source install.sh cu117  # install nle with CUDA 11.7
 #
 # To uninstall and remove the nle environment:
 # $ conda remove -n nle --all
 
 # Check input
 if [ -z $1 ]; then
-    echo "ERROR: Please provide CUDA information, available options are [cpu,cu102,cu113]"
+    echo "ERROR: Please provide CUDA information, available options are [cpu,cu117,cu118]"
     return 1
 fi
 
 # Torch related dependency versions
-PYTORCH_VERSION=1.12.1
-PYG_VERSION=2.0.4
+PYTORCH_VERSION=2.0.0
+PYG_VERSION=2.3.0
 
 # Set CUDA variable (use CPU if not set)
 CUDA_VERSION=${1:-cpu}
 echo "CUDA_VERSION=${CUDA_VERSION}"
 case $CUDA_VERSION in
     cpu)
-        TORCH_OPT="cpuonly"
+        TORCH_OPT="cpuonly -c pytorch"
         ;;
-    cu102)
-        TORCH_OPT="cudatoolkit=10.2"
+    cu117)
+        TORCH_OPT="pytorch-cuda=11.7 -c pytorch -c nvidia"
         ;;
-    cu113)
-        TORCH_OPT="cudatoolkit=11.3"
+    cu118)
+        TORCH_OPT="pytorch-cuda=11.8 -c pytorch -c nvidia"
         ;;
     *)
         echo "ERROR: Unrecognized CUDA_VERSION=${CUDA_VERSION}"
@@ -43,8 +43,8 @@ conda create -n nle python=3.8 -y
 conda activate nle
 
 # Install CUDA enabled dependencies
-conda install pytorch=${PYTORCH_VERSION} torchvision ${TORCH_OPT} -c pytorch -y
-pip install "torch-geometric[full]"==${PYG_VERSION} torch-scatter torch-sparse torch-cluster -f https://data.pyg.org/whl/torch-${PYTORCH_VERSION}+${CUDA_VERSION}.html
+conda install pytorch=${PYTORCH_VERSION} torchvision torchaudio ${TORCH_OPT} -y
+conda install pyg=${PYG_VERSION} -c pyg -y
 
 # Finally, install nle
 pip install -e .
