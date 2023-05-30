35d230a9ae832ce64897fd2ff1d79ed5c54d86e1
Remy <liurenmi@msu.edu>
2023-05-29 21:42:43 -0400

    create pyg dataset object

---
+++ src/nleval/dataset_pyg.py
@@ -0,0 +1,75 @@
+"""PyTorch Geometric dataset object."""
+import os.path as osp
+
+import torch
+from torch_geometric.data import InMemoryDataset
+
+from nleval import __data_version__
+from nleval.typing import Callable, List, Optional
+from nleval.util.dataset_constructors import default_constructor
+
+
+class OpenBiomedNetBench(InMemoryDataset):
+    """PyTorch Geometric default dataset construct.
+
+    Args:
+        root: Root directory of the dataset to be saved.
+        network: Name of the network to use.
+        label: Name of the gene annotation label to use.
+        selected_genes: An optional list of genes. When supplied, will be used
+            to filter out genes in the label in addition to the filtering based
+            on network genes.
+        data_version: Version of the OpenBiomedNetBench data to use. If not
+            specified, will use the current (archived) release. If specified as
+            "latest", then download data from source and process them from
+            scratch.
+        transform: PyG transformation to be applied.
+        pre_transform: PyG transformation to be applied before saving.
+
+    """
+
+    def __init__(
+        self,
+        root: str,
+        network: str,
+        label: str,
+        *,
+        selected_genes: Optional[List[str]] = None,
+        data_version: Optional[str] = None,
+        transform: Optional[Callable] = None,
+        pre_transform: Optional[Callable] = None,
+    ):
+        self.network = network
+        self.label = label
+        self.name = f"{network}-{label}"
+        self.selected_genes = selected_genes
+        self.data_version = data_version or __data_version__
+
+        super().__init__(root, transform, pre_transform)
+        self.data, self.slices = torch.load(self.processed_paths[0])
+
+    def __repr__(self) -> str:
+        paramstr = f"{len(self):,}, network={self.network}, label={self.label}"
+        return f"{self.__class__.__name__}({paramstr})"
+
+    @property
+    def processed_dir(self) -> str:
+        return osp.join(self.root, self.__class__.__name__, self.name, "processed")
+
+    @property
+    def processed_file_names(self) -> str:
+        "data.pt"
+
+    def process(self):
+        dataset = default_constructor(
+            self.root,
+            version=self.data_version,
+            graph_name=self.network,
+            label_name=self.label,
+        )
+        data = dataset.to_pyg_data()
+
+        if self.pre_transform is not None:
+            data = self.pre_transform(data)
+
+        torch.save(self.collate([data]), self.processed_paths[0])
