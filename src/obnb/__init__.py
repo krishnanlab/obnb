a0d7af2c06d29cebb6de2a041660dbd3ec5b4668
Remy <liurenmi@msu.edu>
2023-06-08 15:14:52 -0400

    make a simple version checker

--- src/obnb/__init__.py
+++ src/obnb/__init__.py
@@ -1,11 +1,14 @@
 """Collection of network learning evaluation utilities."""
 from obnb import graph, label, model_trainer
 from obnb.dataset import Dataset
+from obnb.util.checkers import checkVersion
 from obnb.util.logger import config_logger, get_logger
 
 __version__ = "0.1.0-dev1"
 __data_version__ = "nledata-v0.1.0-dev6"
 __all__ = ["Dataset", "graph", "label", "model_trainer"]
+
+checkVersion(__version__)
 
 # Configure logger setting and create global logger
 config_logger()
