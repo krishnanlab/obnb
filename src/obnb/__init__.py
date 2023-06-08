c9fca95f79df967adf2e060346d572be8a06e5cd
Remy Liu <36778645+RemyLau@users.noreply.github.com>
2023-06-08 13:55:11 -0400

    minor fix: bump2vec version (#426)

--- src/obnb/__init__.py
+++ src/obnb/__init__.py
@@ -3,7 +3,7 @@ from obnb import graph, label, model_trainer
 from obnb.dataset import Dataset
 from obnb.util.logger import config_logger, get_logger
 
-__version__ = "0.1.0-dev8"
+__version__ = "0.1.0-dev1"
 __data_version__ = "nledata-v0.1.0-dev6"
 __all__ = ["Dataset", "graph", "label", "model_trainer"]
 
