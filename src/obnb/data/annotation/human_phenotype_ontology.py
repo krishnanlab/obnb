9d44569f0d6ae94cb7046f22522f025d5bfb290e
Remy <liurenmi@msu.edu>
2023-06-08 13:22:37 -0400

    update hpo annotation parsing

--- src/obnb/data/annotation/human_phenotype_ontology.py
+++ src/obnb/data/annotation/human_phenotype_ontology.py
@@ -16,12 +16,12 @@ class HumanPhenotypeOntologyAnnotation(BaseAnnotationData):
     please refer to the HPO annotation
     `download <https://hpo.jax.org/app/data/annotations>`_ page
 
-    **[Last updated: 2023-02-12]**
+    **[Last updated: 2023-06-08]**
 
     Args:
         root: Root directory of the data.
         data_sources: List of evidene types to be considered. If not set,
-            then use the default channels (OMIM and Orphanet).
+            then use the default channels ("OMIM" and "ORPHA").
 
     """
 
@@ -42,13 +42,7 @@ class HumanPhenotypeOntologyAnnotation(BaseAnnotationData):
 
     @property
     def data_sources(self) -> List[str]:
-        if self._data_sources is None:
-            return [
-                "mim2gene",  # OMIM
-                "orphadata",  # Orphanet
-            ]
-        else:
-            return self._data_sources
+        return self._data_sources
 
     def load_processed_data(self):
         path = self.raw_file_path(0)
@@ -66,21 +60,23 @@ class HumanPhenotypeOntologyAnnotation(BaseAnnotationData):
                 "gene_symbol",
                 "term_id",
                 "term_name",
-                "freq_row",
-                "freq_hpo",
-                "info",
-                "source",
+                "frequency",
                 "disease_id",
             ],
         )
         annot_df["gene_id"] = annot_df["gene_id"].astype(str)
 
         # Select specified channels
-        evidence_str = pprint.pformat(self.data_sources)
-        self.plogger.info(f"Subsetting annotations to evidences:\n{evidence_str}")
-        ind = annot_df["source"].isin(self.data_sources)
-        self.plogger.info(f"{ind.sum():,} (out of {ind.shape[0]:,}) entries selected")
-        annot_df = annot_df[ind]
+        if self.data_sources is not None:
+            evidence_str = pprint.pformat(self.data_sources)
+            self.plogger.info(f"Subsetting annotations to evidences:\n{evidence_str}")
+
+            annot_df["source"] = annot_df["disease_id"].str.split(":", expand=True)[0]
+            ind = annot_df["source"].isin(self.data_sources)
+            self.plogger.info(
+                f"{ind.sum():,} (out of {ind.shape[0]:,}) entries selected",
+            )
+            annot_df = annot_df[ind]
 
         # Save attributes
         self.data = annot_df[["gene_id", "term_id"]].copy()
