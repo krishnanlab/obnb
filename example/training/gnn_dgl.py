c84dca23d43027775474556343ab2e951fb97480
Remy <liurenmi@msu.edu>
2023-06-09 15:29:52 -0400

    create dgl gnn training example

--- example/training/gnn_dgl.py
+++ example/training/gnn_dgl.py
@@ -1,13 +1,38 @@
 import argparse
+import warnings
 
+import dgl.nn as dglnn
 import numpy as np
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
-import torch_geometric.nn as pygnn
 
-from obnb.dataset import OpenBiomedNetBenchPyG
+from obnb.dataset import OpenBiomedNetBenchDGL
 from obnb.metric import log2_auprc_prior
 from obnb.util.logger import log
+
+# Ignore TypedStorage warnings https://github.com/dmlc/dgl/pull/5656
+warnings.simplefilter("ignore")
+
+
+class GCN(nn.Module):
+    def __init__(self, in_dim: int, out_dim: int, hid_dim: int, num_layers: int):
+        super().__init__()
+
+        self.layers = nn.ModuleList()
+
+        self.layers.append(dglnn.GraphConv(in_dim, hid_dim, activation=F.relu))
+
+        for _ in range(num_layers - 2):
+            self.layers.append(dglnn.GraphConv(hid_dim, hid_dim, activation=F.relu))
+
+        self.layers.append(dglnn.GraphConv(hid_dim, out_dim))
+
+    def forward(self, g):
+        h = g.ndata["feat"]
+        for layer in self.layers:
+            h = layer(g, h)
+        return h
 
 
 def parse_args():
@@ -26,11 +51,11 @@ def parse_args():
     return parser.parse_args()
 
 
-def train_epoch(gnn, data, optimizer):
+def train_epoch(gnn, g, optimizer):
     gnn.train()
-    pred = gnn(data.x, data.edge_index)
-    mask = data.train_mask[:, 0]
-    loss = F.binary_cross_entropy_with_logits(pred[mask], data.y[mask])
+    pred = gnn(g)
+    mask = g.ndata["train_mask"][:, 0]
+    loss = F.binary_cross_entropy_with_logits(pred[mask], g.ndata["label"][mask])
 
     optimizer.zero_grad()
     loss.backward()
@@ -40,14 +65,14 @@ def train_epoch(gnn, data, optimizer):
 
 
 @torch.no_grad()
-def test_epoch(gnn, data):
+def test_epoch(gnn, g):
     gnn.eval()
-    pred = gnn(data.x, data.edge_index)
+    pred = gnn(g)
 
     scores = []
     for split in ["train", "val", "test"]:
-        mask = getattr(data, f"{split}_mask")[:, 0]
-        scores.append(log2_auprc_prior(data.y[mask], pred[mask]))
+        mask = g.ndata[f"{split}_mask"][:, 0]
+        scores.append(log2_auprc_prior(g.ndata["label"][mask], pred[mask]))
 
     return scores
 
@@ -56,16 +81,16 @@ def main():
     args = parse_args()
 
     # Set up data
-    dataset = OpenBiomedNetBenchPyG(args.root, network=args.network, label=args.label)
-    data = dataset[0]
-    print(f"Graph: {data}")
+    dataset = OpenBiomedNetBenchDGL(args.root, network=args.network, label=args.label)
+    dglgraph = dataset[0]
+    print(f"Graph: {dglgraph}")
 
     # Set up model and optimizer
-    gnn = pygnn.GCN(
-        in_channels=data.x.shape[1],
-        out_channels=data.y.shape[1],
-        hidden_channels=args.hidden_channels,
-        num_layers=args.num_layers,
+    gnn = GCN(
+        dglgraph.ndata["feat"].shape[1],
+        dglgraph.ndata["label"].shape[1],
+        args.hidden_channels,
+        args.num_layers,
     )
     print(f"GNN: {gnn}")
     optimizer = torch.optim.Adam(gnn.parameters(), lr=args.lr, weight_decay=args.wd)
@@ -74,8 +99,8 @@ def main():
     best_epoch = 0
     best_val_apop = best_test_apop = -np.inf
     for epoch in range(1, args.epochs + 1):
-        loss = train_epoch(gnn, data, optimizer)
-        train_apop, val_apop, test_apop = test_epoch(gnn, data)
+        loss = train_epoch(gnn, dglgraph, optimizer)
+        train_apop, val_apop, test_apop = test_epoch(gnn, dglgraph)
 
         if val_apop > best_val_apop:
             best_epoch, best_val_apop, best_test_apop = epoch, val_apop, test_apop
