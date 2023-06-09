af33cd49cd19bc5d9222c6442e87ef4e2d55a589
Remy <liurenmi@msu.edu>
2023-06-09 11:33:32 -0400

    update exmaple gnn scripts

--- example/gcn_studybias_holdout.py
+++ example/gcn_studybias_holdout.py
@@ -7,6 +7,8 @@ from obnb.label.split import RatioPartition
 from obnb.metric import auroc
 from obnb.model_trainer.gnn import SimpleGNNTrainer
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
 # Load dataset (with sparse graph)
 g, lsc, converter = load_data(
     sparse=True,
@@ -14,30 +16,29 @@ g, lsc, converter = load_data(
     filter_holdout_split=True,
 )
 splitter = RatioPartition(0.6, 0.2, 0.2, ascending=False, property_converter=converter)
+dataset = Dataset(graph=g, label=lsc, splitter=splitter)
+data = dataset.to_pyg_data(device=device)
+
+feat_dim = data.x.shape[1]
 n_tasks = len(lsc.label_ids)
-print(f"{n_tasks=}\n")
+print(f"{feat_dim=}\n{n_tasks=}\n")
 
-# Select model
-mdl = GCN(in_channels=1, hidden_channels=64, num_layers=5, out_channels=n_tasks)
-
-# Setup trainer, use auroc as the evaluation metric
+# Setup model and trainer, use auroc as the evaluation metric
 metrics = {"auroc": auroc}
-device = "cuda" if torch.cuda.is_available() else "cpu"
 trainer = SimpleGNNTrainer(
-    metrics,
+    metrics=metrics,
     device=device,
     metric_best="auroc",
     epochs=100,
     lr=0.1,
     log_path="test_log/gcn/run.log",
 )
+mdl = GCN(in_channels=feat_dim, hidden_channels=64, num_layers=5, out_channels=n_tasks)
 
-dataset = Dataset(graph=g, label=lsc, splitter=splitter)
 results = trainer.train(mdl, dataset)
 print(f"\nBest results:\n{results}\n")
 
 # Check to see if the model is rewinded back to the best model correctly
-data = dataset.to_pyg_data(device=device)
 y_pred = mdl(data.x, data.edge_index).detach().cpu().numpy()
 for mask_name in "train", "val", "test":
     mask = dataset.masks[mask_name][:, 0]
