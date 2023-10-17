e8f2ab2d9f24f9b326686d9c608136af69cf17ae
Remy <liurenmi@msu.edu>
2023-10-17 12:27:00 -0400

    minor test refactor; add tests for transformd datasets in pyg and dgl

--- test/transform/node_feature.py
+++ test/transform/node_feature.py
@@ -4,12 +4,32 @@ from obnb import Dataset
 from obnb.label import LabelsetCollection
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture
 def toy_dataset_1(toy_graph_1):
     lsc = LabelsetCollection()
     lsc.add_labelset(["a", "b", "c", "d", "e"], "label1")
     lsc.add_labelset(["d", "e", "f", "g", "h"], "label2")
     return toy_graph_1, lsc
+
+
+def _test_transform_nodefeat_template(transform, toy_dataset):
+    graph, label = toy_dataset
+
+    if transform == "Constant":
+        emb_dim = 4
+    elif transform == "Adj":
+        emb_dim = None
+    else:
+        emb_dim = 4
+
+    dataset = Dataset(
+        graph=graph,
+        label=label,
+        transform=transform,
+        transform_kwargs=dict(dim=emb_dim, as_feature=True),
+        auto_generate_feature=None,
+    )
+    print(f"{transform}:\n{dataset.feature.mat}\n")
 
 
 @pytest.mark.parametrize(
@@ -27,16 +47,7 @@ def toy_dataset_1(toy_graph_1):
     ],
 )
 def test_transform_nodefeat_basic(transform, toy_dataset_1):
-    graph, label = toy_dataset_1
-    emb_dim = 1 if transform == "Constant" else 4
-    dataset = Dataset(
-        graph=graph,
-        label=label,
-        transform=transform,
-        transform_kwargs=dict(dim=emb_dim, as_feature=True),
-        auto_generate_feature=None,
-    )
-    print(f"{transform}:\n{dataset.feature.mat}\n")
+    _test_transform_nodefeat_template(transform, toy_dataset_1)
 
 
 @pytest.mark.parametrize(
@@ -52,13 +63,25 @@ def test_transform_nodefeat_basic(transform, toy_dataset_1):
 )
 @pytest.mark.ext
 def test_transform_nodefeat_ext(transform, toy_dataset_1):
+    _test_transform_nodefeat_template(transform, toy_dataset_1)
+
+
+@pytest.mark.ext
+def test_transform_nodefeat_converted(toy_dataset_1):
     graph, label = toy_dataset_1
-    emb_dim = 1 if transform == "Constant" else 4
+
     dataset = Dataset(
         graph=graph,
         label=label,
-        transform=transform,
-        transform_kwargs=dict(dim=emb_dim, as_feature=True),
+        transform="OneHotLogDeg",
+        transform_kwargs=dict(dim=4, as_feature=True),
         auto_generate_feature=None,
     )
-    print(f"{transform}:\n{dataset.feature.mat}\n")
+
+    dataset_pyg = dataset.to_pyg_data()
+    assert "nodefeat_OneHotLogDeg" in dataset_pyg
+    print(f"PyG dataset:\n{dataset_pyg}\n")
+
+    dataset_dgl = dataset.to_dgl_data()
+    assert "nodefeat_OneHotLogDeg" in dataset_dgl.ndata
+    print(f"DGL dataset:\n{dataset_dgl}\n")
